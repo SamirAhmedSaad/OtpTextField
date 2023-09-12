e0bdb439a651b0105bf1cfa1e6a840bd557ecbfd
Samir.Hayan <Ahmed@saad911>
2023-09-12 23:27:15 +0300

    - Release 1.0.1

--- otpTextField/build.gradle.kts
+++ otpTextField/build.gradle.kts
@@ -69,7 +69,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.SamirAhmedSaad"
             artifactId = "OtpTextField"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
